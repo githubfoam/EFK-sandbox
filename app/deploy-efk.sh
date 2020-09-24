9d31ac6dcf4797312baee9f9e45721afcf7afefa
githubfoam <lequestionis@gmail.com>
2020-09-24 21:23:24 +0300

    как`им

--- app/deploy-efk.sh
+++ app/deploy-efk.sh
@@ -7,8 +7,8 @@ set -o xtrace
 
 echo "=============================EFK Elastic Fluentd Kibana============================================================="
 
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo update
+# helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# helm repo update
 
 # helm install elasticsearch stable/elasticsearch #WARNING: This chart is deprecated
 # sleep 10
@@ -20,3 +20,10 @@ helm repo update
 # kubectl apply -f app/counter.yaml
 
 # curl kibana dashboard
+
+# Install released version using Helm repository
+# https://hub.helm.sh/charts/elastic/elasticsearch
+# Add the Elastic Helm charts repo: 
+helm repo add elastic https://helm.elastic.co
+# with Helm 2: 
+helm install --name elasticsearch --version 7.9.2 elastic/elasticsearch
